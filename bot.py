6b13681165144c573b1a4befcec75d60b293a132
Prokopiy Lazarev <73588105+Kosaka01@users.noreply.github.com>
2025-09-08 09:58:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,21 +5,17 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# --- ЛОГИ ---
 logging.basicConfig(level=logging.INFO)
 
-# --- НАСТРОЙКИ ---
-TELEGRAM_TOKEN = "mytoken"
-GOOGLE_CREDENTIALS_FILE = "service-account.json"  # скачанный JSON
-SPREADSHEET_NAME = "predictions"  # название таблицы
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GOOGLE_CREDENTIALS_FILE = json.loads(os.getenv("GOOGLE_CREDS"))
+SPREADSHEET_NAME = "predictions" 
 
-# --- ДОСТУП К GOOGLE SHEETS ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_CREDENTIALS_FILE, scope)
 client = gspread.authorize(creds)
 sheet = client.open(SPREADSHEET_NAME).sheet1
 
-# --- КОМАНДЫ ---
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я бот с предсказаниями 🔮 Напиши /predict, чтобы получить своё будущее!")
 
@@ -31,7 +27,6 @@ async def predict(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("Увы, предсказаний пока нет 😢")
 
-# --- ЗАПУСК ---
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
 
@@ -42,3 +37,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
